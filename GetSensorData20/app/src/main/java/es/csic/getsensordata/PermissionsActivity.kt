dbc5e0707714c4e95e596429f5384a4344203379
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2019-09-07 10:11:33 +0200

    Add empty activities for permission checking

---
+++ GetSensorData20/app/src/main/java/es/csic/getsensordata/PermissionsActivity.kt
@@ -0,0 +1,12 @@
+package es.csic.getsensordata
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PermissionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permissions)
+    }
+}
